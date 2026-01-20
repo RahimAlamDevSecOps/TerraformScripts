54aa91afbc9e84c92b17a075feca93aaf1fb36bb
RahimAlamDevSecOps <133257112+RahimAlamDevSecOps@users.noreply.github.com>
2026-01-20 14:25:39 +0530

    ec2-3

---
+++ EC2-3.tf
@@ -0,0 +1,61 @@
+provider "aws" {
+  region = "ca-central-1"
+}
+
+resource "aws_instance" "windows" {
+  ami           = "ami-085f043560da76e08"
+  instance_type = "t3.micro"
+  key_name      = "rahimkey"
+
+  vpc_security_group_ids = [aws_security_group.windows_sg.id]
+
+  root_block_device {
+    volume_size = 8
+  }
+
+  tags = {
+    Name = "LinuxInstance"
+  }
+}
+
+resource "aws_security_group" "windows_sg" {
+  name        = "windows-sg"
+  description = "Allow SSH and HTTP access"
+
+  ingress {
+    description = "RDP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Linux-SG"
+  }
+}
+
+output "instance_public_ip" {
+    description = "The public Ip adress of the EC2 instance"
+    value = aws_instance.windows.public_ip
+  }
+  output "instance_private_ip" {
+    description = "The private ip address of the EC2 instance"
+    value = aws_instance.windows.private_ip
+    
+  }
